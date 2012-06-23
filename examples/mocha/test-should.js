1cdd533a7cb908fb835fedbca9c8d845dd1d1974
Eric Kryski <e.kryski@gmail.com>
2012-06-23 14:29:16 -0600

    added chai example and made vanilla mocha

--- examples/mocha/test-should.js
+++ examples/mocha/test-should.js
@@ -8,26 +8,19 @@ describe('BlogPost test', function() {
 	it('Should be published at the current time', function() {
 		var now = new Date(),
 			post = new BlogPost('Hello', 'Hello world');
-		assert(post.date.getTime() == now.getTime());
+		post.date.getTime().should.equal(now.getTime());
 	});
 
 	it('Should throw an exception', function() {
 		var post = new BlogPost('Hello', 'Hello world');
-		try {
-			post.toString();
-			assert(false);
-		}
-		catch(e) {
-			assert(true);
-			assert(e == "This blog post is not published");
-		}
+		post.toString.should.throw();
 	});
 
 	it('Generates some neat HTML', function() {
 		var now = new Date(),
 			post = new BlogPost('Hello', 'Hello world');
 		post.publish();
-		assert(post.toString() == "<h1>Hello</h1>" +
+		post.toString().should.equal("<h1>Hello</h1>" +
 			"<h6>Published on " + now.toString() + "</h6>" +
 			"<p>Hello world</p>");
 	});
